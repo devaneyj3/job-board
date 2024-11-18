aeb5d8e8b262a222e010f944e4636cb7ccfcf373
jordandevaney <jordandevaney28@gmail.com>
2024-11-18 16:23:51 -0500

    query variables

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -1,7 +1,24 @@
 import { GraphQLClient, gql } from "graphql-request";
 
 const client = new GraphQLClient("http://localhost:9000/graphql");
-
+export async function getJob(id) {
+	const query = gql`
+		query jobById($id: ID!) {
+			job(id: $id) {
+				id
+				title
+				date
+				description
+				company {
+					id
+					name
+				}
+			}
+		}
+	`;
+	const data = await client.request(query, { id });
+	return data.job;
+}
 export async function getJobs() {
 	const query = gql`
 		query {
