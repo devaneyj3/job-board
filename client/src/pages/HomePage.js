5bab56ed931449e25971c4d9cab4db8bde43a348
jordandevaney <jordandevaney28@gmail.com>
2024-11-18 15:39:03 -0500

    graphql request

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,14 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import JobList from "../components/JobList";
+import { getJobs } from "../lib/graphql/queries";
 
+getJobs().then((jobs) => console.log(jobs));
 function HomePage() {
-  return (
-    <div>
-      <h1 className="title">
-        Job Board
-      </h1>
-      <JobList jobs={jobs} />
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="title">Job Board</h1>
+			<JobList jobs={jobs} />
+		</div>
+	);
 }
 
 export default HomePage;
