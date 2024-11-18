afb1af04a1bf80c7b8ce8cfd7ca4099205c8ebc9
jordandevaney <jordandevaney28@gmail.com>
2024-11-18 15:51:51 -0500

    put jobs in state

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import JobList from "../components/JobList";
 import { getJobs } from "../lib/graphql/queries";
 
-getJobs().then((jobs) => console.log(jobs));
 function HomePage() {
+	const [jobs, setJobs] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await getJobs();
+			setJobs(data);
+		};
+		fetchData();
+	}, []);
 	return (
 		<div>
 			<h1 className="title">Job Board</h1>
