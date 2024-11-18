a452a8aaf2d6ada4dbcd4d31155691cd8c2ae5e7
jordandevaney <jordandevaney28@gmail.com>
2024-11-18 12:21:27 -0500

    field resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,4 +3,12 @@ export const resolvers = {
 	Query: {
 		jobs: () => getJobs(),
 	},
+
+	Job: {
+		date: ({ createdAt }) => toIsoDate(createdAt),
+	},
 };
+
+function toIsoDate(value) {
+	return value.slice(0, "yyyy-mm-dd".length);
+}
