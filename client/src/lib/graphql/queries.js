ecfcacd9cd293140b84cda9080e1cb4aca510e5d
jordandevaney <jordandevaney28@gmail.com>
2024-12-18 11:12:19 -0500

    authenticate request on the client

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -1,6 +1,41 @@
 import { GraphQLClient, gql } from "graphql-request";
+import { getAccessToken } from "../auth";
 
-const client = new GraphQLClient("http://localhost:9000/graphql");
+const client = new GraphQLClient("http://localhost:9000/graphql", {
+	headers: () => {
+		const accessToken = getAccessToken();
+		if (accessToken) {
+			return { Authorization: `Bearer ${accessToken}` };
+		}
+		return {};
+	},
+});
+
+export async function createJob({ title, description }) {
+	const mutation = gql`
+		mutation CreateJob($input: CreateJobInput!) {
+			job: createJob(input: $input) {
+				id
+			}
+		}
+	`;
+	const { job } = await client.request(mutation, {
+		input: { title, description },
+	});
+	return job;
+}
+
+export async function deletJob(id) {
+	const mutation = gql`
+		mutation {
+			deleteJob(id: id) {
+				title
+			}
+		}
+	`;
+	const { job } = await client.request(mutation, id);
+	return job;
+}
 export async function getJob(id) {
 	const query = gql`
 		query jobById($id: ID!) {
